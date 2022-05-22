5e88a380ed6277d4060962d1f3bf34d1d8883fe0
Abdallah Muftah <scar.naruto@hotmail.com>
2022-05-22 20:26:52 +0400

    Update domain.sh

--- assets/domain.sh
+++ assets/domain.sh
@@ -10,17 +10,17 @@ echo "*          with Let's Encrypt            *"
 echo "******************************************"
 echo ""
 #
-read -p 'Set Web Domain (Example: 127.0.0.1 [Not trailing slash!]) : ' domain
-read -p 'Email for Lets Encrypt SSL : ' email
+read -p 'Set Web Domain (Example: 127.0.0.1 [Not trailing slash!]) : ' sdomain
+read -p 'Email for Lets Encrypt SSL : ' semail
 #
 mkdir /var/www/html/$domain
 wget -P /etc/apache2/sites-available https://raw.githubusercontent.com/abdomuftah/UbuntuServer/main/assets/Example.conf
-mv /etc/apache2/sites-available/Example.conf /etc/apache2/sites-available/$domain.conf
-sed -i "s/example.com/$domain/g" /etc/apache2/sites-available/$domain.conf
-wget -P /var/www/html/$domain https://raw.githubusercontent.com/abdomuftah/UbuntuServer/main/assets/index.php
+mv /etc/apache2/sites-available/Example.conf /etc/apache2/sites-available/$sdomain.conf
+sed -i "s/example.com/$sdomain/g" /etc/apache2/sites-available/$sdomain.conf
+wget -P /var/www/html/$sdomain https://raw.githubusercontent.com/abdomuftah/UbuntuServer/main/assets/index.php
 a2ensite $domain
 systemctl restart apache2
-certbot --noninteractive --agree-tos --no-eff-email --cert-name $domain --apache --redirect -d $domain -m $email
+certbot --noninteractive --agree-tos --no-eff-email --cert-name $sdomain --apache --redirect -d $sdomain -m $semail
 systemctl restart apache2.service
 certbot renew --dry-run
 systemctl restart apache2.service
@@ -32,6 +32,6 @@ echo "Join My Discord Server "
 echo "https://discord.snyt.xyz"
 echo "##################################"
 echo " ypur Domain is now ready  : "
-echo "	 https://$domain 	"
+echo "	 https://$sdomain 	"
 #
 exit
